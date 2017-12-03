65706353248cfc13de062628e911a695745b15fa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-03 21:02:24 +0700

    new_transaction

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,34 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
+    # Blockchain class
 
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
+    def new_block(self, sender, recipient, amount):
         pass
 
     def new_transaction(self):
-        # Adds new transaction to the list of transactions
-        pass
+         """
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <str> Amount
+        :return: <init> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
@@ -18,6 +36,6 @@ class Blockchain(object):
         pass
 
     @property
-    def last_block(self):
+    def last_block['index'] + 1(self):
         # Returns the last block of the chain
         pass
