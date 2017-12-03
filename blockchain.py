489d4d13796c2c9de09b7f69b836dd62e06f978c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-03 21:19:55 +0700

    Flask App

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 from time import time
 from uuid import uuid4
 
+from flask import Flase
+
 class Blockchain(object):
     # Blockchain class
 
@@ -102,3 +104,30 @@ class Blockchain(object):
     def last_block(self):
         # Returns the last block of the chain
         return self.chain[-1]
+
+
+app =  Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=["GET"])
+def mine():
+    return "We will mine a new block"
+
+@app.route('/transactions/new', methods=["POST"])
+def new_transaction():
+    return "We will add a new transaction"
+
+@app.route('/chain', methods=["GET"])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
